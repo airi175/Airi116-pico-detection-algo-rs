af92bbf7386495b5f954667cfa7d95b2c42bca88
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-05-13 09:16:06 +0000

    docs(readme): update

--- benches/detector.rs
+++ benches/detector.rs
@@ -1,4 +1,11 @@
 #[path = "./common/macros.rs"]
 mod macros;
 
-use st
+use std::fs;
+
+use criterion::{black_box, Criterion};
+
+use image;
+use pico_detect::{Detector, Square};
+
+pub fn bench_load(c: &
