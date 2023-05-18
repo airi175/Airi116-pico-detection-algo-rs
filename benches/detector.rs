488f6a966c260dd5091e6c9dcbf507f031c8ac5d
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-05-18 14:54:01 +0000

    New translations strings.xml (Hebrew)

--- benches/detector.rs
+++ benches/detector.rs
@@ -8,4 +8,7 @@ use criterion::{black_box, Criterion};
 use image;
 use pico_detect::{Detector, Square};
 
-pub fn bench_load(c: &
+pub fn bench_load(c: &mut Criterion) {
+    let model_data = fs::read(model_path!(facefinder)).unwrap();
+
+    c.bench_function("Detector::load", |b| {
