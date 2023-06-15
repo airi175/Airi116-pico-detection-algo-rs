d9bdcd2741e53917713949a1c538843c4ae6ad8e
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-06-15 16:50:22 +0000

    Add jscs-jsdoc as a dev-dependency

--- benches/perturbator.rs
+++ benches/perturbator.rs
@@ -4,4 +4,8 @@ mod macros;
 use criterion::{black_box, BenchmarkId, Criterion, Throughput};
 
 use pico_detect::{Square, perturbate::Perturbator};
-use rand::Seed
+use rand::SeedableRng;
+use rand_xoshiro::Xoroshiro128PlusPlus;
+
+pub fn bench_run(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Perturbat
