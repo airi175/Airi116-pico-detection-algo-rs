6ad2da58ee603eacadb2ac5414e53c98133fe191
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-06-15 17:09:58 +0000

    Fix JSHint warnings

--- benches/perturbator.rs
+++ benches/perturbator.rs
@@ -8,4 +8,9 @@ use rand::SeedableRng;
 use rand_xoshiro::Xoroshiro128PlusPlus;
 
 pub fn bench_run(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Perturbat
+    let mut group = c.benchmark_group("Perturbator::run");
+
+    group.sample_size(10000);
+
+    let perturbator = Perturbator::default();
+    let mut rng = Xoroshiro128PlusPlus::s
