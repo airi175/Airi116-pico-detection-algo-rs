a0f7a0cc1c7bbef2ac5c5cfb69d521ab5a963093
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-06-15 22:04:02 +0000

    bonding: fix destruction of bond with devices different from arphrd_ether

--- benches/perturbator.rs
+++ benches/perturbator.rs
@@ -13,4 +13,9 @@ pub fn bench_run(c: &mut Criterion) {
     group.sample_size(10000);
 
     let perturbator = Perturbator::default();
-    let mut rng = Xoroshiro128PlusPlus::s
+    let mut rng = Xoroshiro128PlusPlus::seed_from_u64(42);
+
+    let init = Square::new(100, 100, 100).into();
+
+    for n in [15, 19, 23, 27, 31].iter() {
+        let id = Ben
