f054105636fc193d7e7a7e06b1b5b3122bdca023
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-07-07 10:50:09 +0000

    Update blog.yml

--- examples/detect-faces/face.rs
+++ examples/detect-faces/face.rs
@@ -1,4 +1,9 @@
 use nalgebra::Point2;
 use pico_detect::Target;
 
-#[derive(Clone,
+#[derive(Clone, Debug)]
+pub struct Face {
+    pub score: f32,
+    pub region: Target,
+    pub shape: Vec<Point2<f32>>,
+    pub pupils: (Point2<f32>, Point2
