e34ee86fca83d52735d1382256dbfb6fb3a03763
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-07-29 08:09:22 +0000

    mm: vmscan: support complete shrinker reclaim

--- examples/detect-faces/models.rs
+++ examples/detect-faces/models.rs
@@ -3,4 +3,9 @@ use std::path::PathBuf;
 #[macro_export]
 macro_rules! model_path {
     ($args:ident, $var:tt, $default:literal) => {
-        crate::mo
+        crate::models::model_path($args.$var.as_ref(), $args.models_dir.as_ref(), $default)
+    };
+}
+
+#[macro_export]
+macro_rules! load
