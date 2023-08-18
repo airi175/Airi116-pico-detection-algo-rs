68ff2faaf738e2f64a5912ea301981a932363c9f
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-08-18 17:20:10 +0000

    Prepare 1.1.6 release

--- src/detect/clusterize.rs
+++ src/detect/clusterize.rs
@@ -7,4 +7,10 @@ use nalgebra::Point2;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Clusterizer {
-    pub intersection_th
+    pub intersection_threshold: f32,
+    pub score_threshold: f32,
+}
+
+impl Clusterizer {
+    #[inline]
+    pub fn intersection_threshold(self, value: f32) -> Self {
