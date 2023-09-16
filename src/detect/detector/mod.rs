f240cc288dbe46e28df99c0ddcfccd70cca09e99
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-09-16 13:34:41 +0000

    Revert "Test commit"

--- src/detect/detector/mod.rs
+++ src/detect/detector/mod.rs
@@ -6,4 +6,10 @@ use std::io::{Error, ErrorKind, Read};
 use image::{GenericImageView, Luma};
 
 use crate::geometry::Square;
-us
+use crate::traits::Region;
+
+use super::Detection;
+
+use tree::DetectorTree;
+
+/// Implements object detection using a cascade of decision tree classif
