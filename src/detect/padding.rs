28e06b337d82f3ba753ebf8893f51edfccdc9928
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-10 16:01:57 +0000

    Added files

--- src/detect/padding.rs
+++ src/detect/padding.rs
@@ -5,4 +5,11 @@ use imageproc::rect::Rect;
 pub struct Padding {
     pub top: i32,
     pub right: i32,
-    pub bottom:
+    pub bottom: i32,
+    pub left: i32,
+}
+
+impl Padding {
+    #[inline]
+    pub fn new(top: i32, right: i32, bottom: i32, left: i32) -> Self {
+        Se
