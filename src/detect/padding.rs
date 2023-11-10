e8ae8d1cba4b62c23619607db9bcb324412122b4
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-10 14:34:30 +0000

    Linux 3.4.43

--- src/detect/padding.rs
+++ src/detect/padding.rs
@@ -1,2 +1,8 @@
 use image::GenericImageView;
-use imageproc::rect
+use imageproc::rect::Rect;
+
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct Padding {
+    pub top: i32,
+    pub right: i32,
+    pub bottom:
