335e5d1cc8c7d2d02356eeaf870d9d5e7eefb0ef
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-21 17:57:15 +0000

    add regression test

--- src/geometry/iou.rs
+++ src/geometry/iou.rs
@@ -3,4 +3,6 @@ use std::cmp::{min, max};
 use crate::traits::Region;
 
 #[inline]
-pub fn intersecti
+pub fn intersection_over_union<R: Region>(r1: R, r2: R) -> Option<f32> {
+    let left = max(r1.left(), r2.left());
+    let top = max(r1.top
