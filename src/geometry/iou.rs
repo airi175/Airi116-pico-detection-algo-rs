53ee8cda305b63d19b6e1d412feeaada93d965ec
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-23 19:37:52 +0000

    Create version.txt

--- src/geometry/iou.rs
+++ src/geometry/iou.rs
@@ -5,4 +5,8 @@ use crate::traits::Region;
 #[inline]
 pub fn intersection_over_union<R: Region>(r1: R, r2: R) -> Option<f32> {
     let left = max(r1.left(), r2.left());
-    let top = max(r1.top
+    let top = max(r1.top(), r2.top());
+    let right = min(r1.right(), r2.right());
+    let bottom = min(r1.bottom(), r2.bottom());
+
+    if right < left || 
