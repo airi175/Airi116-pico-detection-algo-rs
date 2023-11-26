da74412ff64e86e09cba9bc7a78faee53d706d81
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-26 08:56:59 +0000

    update master

--- src/geometry/iou.rs
+++ src/geometry/iou.rs
@@ -9,4 +9,9 @@ pub fn intersection_over_union<R: Region>(r1: R, r2: R) -> Option<f32> {
     let right = min(r1.right(), r2.right());
     let bottom = min(r1.bottom(), r2.bottom());
 
-    if right < left || 
+    if right < left || bottom < top {
+        return None;
+    }
+
+    let width = (right - left) as u32 + 1;
+    let height = (bottom - top)
