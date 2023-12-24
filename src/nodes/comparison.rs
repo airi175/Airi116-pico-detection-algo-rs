6980f096abd2898b60944e98925b027c5003785d
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-12-24 08:49:10 +0000

    Fix parsing

--- src/nodes/comparison.rs
+++ src/nodes/comparison.rs
@@ -1,4 +1,9 @@
 use image::{GenericImageView, Luma};
 use nalgebra::Point2;
 
-use pixelutil_image::clamp_pixel_unc
+use pixelutil_image::clamp_pixel_unchecked;
+
+#[derive(Debug, PartialEq, Clone, Copy, Default)]
+pub struct ComparisonNode(pub Point2<i8>, pub Point2<i8>);
+
+impl 
