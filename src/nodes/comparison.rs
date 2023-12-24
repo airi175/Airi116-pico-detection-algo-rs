97b365f804ebffe41c47b23e7f74efd058f2d498
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-12-24 09:33:22 +0000

    Automatic i18n update (i18n.meetfranz.com)

--- src/nodes/comparison.rs
+++ src/nodes/comparison.rs
@@ -6,4 +6,7 @@ use pixelutil_image::clamp_pixel_unchecked;
 #[derive(Debug, PartialEq, Clone, Copy, Default)]
 pub struct ComparisonNode(pub Point2<i8>, pub Point2<i8>);
 
-impl 
+impl From<[i8; 4]> for ComparisonNode {
+    #[inline]
+    fn from(data: [i8; 4]) -> Self {
+        let [y0, x0, y1, x1] = d
