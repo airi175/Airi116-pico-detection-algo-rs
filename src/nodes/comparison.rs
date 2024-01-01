8ec3b39f108d4f307c66bc2b25df748c3c4ba789
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-01-01 06:51:58 +0000

    Update CONTRIBUTORS.rst

--- src/nodes/comparison.rs
+++ src/nodes/comparison.rs
@@ -9,4 +9,11 @@ pub struct ComparisonNode(pub Point2<i8>, pub Point2<i8>);
 impl From<[i8; 4]> for ComparisonNode {
     #[inline]
     fn from(data: [i8; 4]) -> Self {
-        let [y0, x0, y1, x1] = d
+        let [y0, x0, y1, x1] = data;
+        Self(Point2::new(x0, y0), Point2::new(x1, y1))
+    }
+}
+
+impl From<[u8; 4]> for ComparisonNode {
+    #[inline]
+    fn fr
