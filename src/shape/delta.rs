d1bbbfcead307e4820ad25796e76c918f6011e76
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-14 14:38:33 +0000

    Get rid of some more trailing whitespace

--- src/shape/delta.rs
+++ src/shape/delta.rs
@@ -1,3 +1,11 @@
 use nalgebra::Vector2;
 
-#[derive(Debug,
+#[derive(Debug, Clone)]
+pub struct ShaperDelta {
+    anchor: usize,
+    value: Vector2<f32>,
+}
+
+impl ShaperDelta {
+    #[inline]
+    pub fn new(anchor: us
