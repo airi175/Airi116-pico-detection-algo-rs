1b9b24380e1632165b03639bdeb912e93d76bef8
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-28 21:26:39 +0000

    mmc: mxs-mmc: fix deadlock caused by recursion loop

--- src/shape/utils.rs
+++ src/shape/utils.rs
@@ -1,3 +1,8 @@
 use std::io::{Error, Read};
 
 use nalgebra::{allocator::Allocator, DefaultAllocator, Dim, Dyn, OMatrix, UninitMatrix, U2};
+
+#[inline]
+pub fn read_shape<R: Read>(mut reader: R, size: usize) -> Result<OMatrix<f32, U2, Dyn>, Error>
+where
+  
