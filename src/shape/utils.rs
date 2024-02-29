04ad82895db0bc17b6c1fff64e2f083d4d52b636
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-29 21:51:20 +0000

    Release 0.5.4

--- src/shape/utils.rs
+++ src/shape/utils.rs
@@ -7,4 +7,10 @@ pub fn read_shape<R: Read>(mut reader: R, size: usize) -> Result<OMatrix<f32, U2
 where
     DefaultAllocator: Allocator<f32, U2, Dyn>,
 {
-    let mut m = UninitMatrix::<f32, U2, Dyn>::uninit(U2, Dyn::from_usize(size))
+    let mut m = UninitMatrix::<f32, U2, Dyn>::uninit(U2, Dyn::from_usize(size));
+
+    let mut buf = [0u8; 4];
+
+    for value in m.iter_mut() {
+        reader.read_exact(&mut buf)?;
+        value.write(
