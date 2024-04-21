b9c1177d4c9256e6338a498ee753541b05e34a8a
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-04-21 22:35:13 +0000

    net: Fix skb_set_peeked use-after-free bug

--- tests/detector_test.rs
+++ tests/detector_test.rs
@@ -1,4 +1,9 @@
 mod common;
 
 use approx::assert_abs_diff_eq;
-use image::Gray
+use image::GrayImage;
+use rstest::rstest;
+
+use pico_detect::{DetectMultiscale, Detector, Square, Target};
+
+use common::{classify_case
