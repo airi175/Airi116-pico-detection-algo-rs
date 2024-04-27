5399e4b315f506bad6487b4fbe2749e3513fb7fc
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-04-27 18:35:57 +0000

    removed unecessary code

--- tests/detector_test.rs
+++ tests/detector_test.rs
@@ -6,4 +6,7 @@ use rstest::rstest;
 
 use pico_detect::{DetectMultiscale, Detector, Square, Target};
 
-use common::{classify_case
+use common::{classify_case, detect_multiscale, detect_multiscale_case, detector};
+
+#[rstest]
+fn test_detector_classify(detector: Detector, classify_c
