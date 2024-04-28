6b5480b9e47abc7ede5d12e6607ac2333b25093f
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-04-28 11:20:30 +0000

    Removed style attribute from pre elements

--- tests/detector_test.rs
+++ tests/detector_test.rs
@@ -9,4 +9,6 @@ use pico_detect::{DetectMultiscale, Detector, Square, Target};
 use common::{classify_case, detect_multiscale, detect_multiscale_case, detector};
 
 #[rstest]
-fn test_detector_classify(detector: Detector, classify_c
+fn test_detector_classify(detector: Detector, classify_case: (GrayImage, Square, Option<f32>)) {
+    let (image, region, score) = classify_case;
+    assert!(detector.classify(&image, 
