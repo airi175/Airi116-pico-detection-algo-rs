502a814131c5672f2345e643d30a4035df547b6e
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-04-30 20:08:20 +0000

    msm: mdss: fix possible out-of-bounds and overflow issue in mdp debugfs

--- tests/localizer_test.rs
+++ tests/localizer_test.rs
@@ -13,4 +13,10 @@ use pico_detect::{Localizer, Square, LocalizePerturbate};
 #[rstest]
 fn test_localizer_localize(
     localizer: Localizer,
-    localize_case: (GrayImage, [(Squar
+    localize_case: (GrayImage, [(Square, Point2<f32>); 2]),
+) {
+    let (image, tests) = localize_case;
+
+    for (region, point) in tests.iter() {
+        assert_abs_diff_eq!(
+       
