f9b518941430d38a7c5f3287cb268e708a7c6cc4
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-04-30 05:31:10 +0000

    Update presentation.html

--- tests/localizer_test.rs
+++ tests/localizer_test.rs
@@ -6,4 +6,6 @@ use rstest::rstest;
 
 use nalgebra::Point2;
 
-use common::{localize_case, lo
+use common::{localize_case, localize_perturbate_case, localizer, localize_perturbate, rng};
+
+use pico_detect::{Localizer, Square, LocalizePertur
