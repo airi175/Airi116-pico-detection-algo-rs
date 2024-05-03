7736a5ba66985a67e60e67507e1f3dffaaf82eba
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-05-03 17:09:33 +0000

    libs update

--- tests/shaper_test.rs
+++ tests/shaper_test.rs
@@ -11,4 +11,7 @@ use pico_detect::{Shaper, Square};
 use common::{shaper, shaper_case};
 
 #[rstest]
-fn test_shaper_predict(shaper: Shaper, shaper_case: (GrayImage, Square, 
+fn test_shaper_predict(shaper: Shaper, shaper_case: (GrayImage, Square, Vec<Point2<f32>>)) {
+    let (image, region, points) = shaper_case;
+
+    for (p1, p2) in shaper.shape(&image, region.into()).iter().zip(points.
