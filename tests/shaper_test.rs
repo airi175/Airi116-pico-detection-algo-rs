9e54ee36093e4fda8cabea3e6fffa3183d3be957
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-05-03 12:40:56 +0000

    chg

--- tests/shaper_test.rs
+++ tests/shaper_test.rs
@@ -6,4 +6,9 @@ use approx::assert_abs_diff_eq;
 use image::GrayImage;
 use nalgebra::Point2;
 
-use pico_detect:
+use pico_detect::{Shaper, Square};
+
+use common::{shaper, shaper_case};
+
+#[rstest]
+fn test_shaper_predict(shaper: Shaper, shaper_case: (GrayImage, Square, 
